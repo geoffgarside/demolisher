2271ee16fe52a9191f8ec4699098288d549d9521
Geoff Garside <geoff@geoffgarside.co.uk>
2009-06-05 00:33:40 +0100

    Add demolisher code and some rdocs

---
+++ lib/demolisher.rb
@@ -0,0 +1,63 @@
+require 'xml'
+
+module Demolisher
+  # Demolish an XML file or XML::Parser object.
+  def self.demolish(file_or_xml_parser)
+    file_or_xml_parser = XML::Parser.file(file_or_xml_parser) if file_or_xml_parser.kind_of?(String)
+    Node.new(file_or_xml_parser.parse, true)
+  end
+  class Node
+    # Creates a new Node object. If the node is not the root node then
+    # the second argument needs to be false.
+    def initialize(xml, is_root = true)
+      @nodes = [xml]
+      @nodes.unshift(nil) unless is_root
+    end
+    # Access an attribute of the current node
+    # Example:
+    #   <addressbook>
+    #     <person rel="friend">
+    #       <firstname>Steve</firstname>
+    #     </person>
+    #   </addressbook>
+    # 
+    #   xml.addressbook do
+    #     xml.person do
+    #       puts "#{xml.firstname} is a #{xml['rel']}"  #=> "Steve is a friend"
+    #     end
+    #   end
+    # 
+    def [](attr_name)
+      @nodes.last.attributes[attr_name]
+    end
+    def method_missing(meth, *args, &block) # :nodoc:
+      xpath = @nodes.last.find(element_from_symbol(meth))
+      return nil if xpath.empty?
+
+      if block_given?
+        xpath.each do |node|
+          @nodes.push(node)
+          yield
+          @nodes.pop
+        end
+      else
+        case meth.to_s
+        when /\?$/
+          !! Regexp.new(/(t(rue)?|y(es)?|1)/i).match(xpath.first.content.strip)
+        else
+          self.class.new(xpath.first, false)
+        end
+      end
+    end
+    def to_s # :nodoc:
+      @nodes.last.content.strip
+    end
+    def element_from_symbol(sym) # :nodoc:
+      "#{is_root_node? ? '/' : nil}#{sym.to_s.gsub(/[^a-z0-9_]/i, '')}"
+    end
+    # Indicates if the current node is the root of the XML document
+    def is_root_node?
+      @nodes.size == 1
+    end
+  end
+end
