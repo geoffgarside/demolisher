0c282936bf32834c6461e6dfa5cebf8f7a43857c
Geoff Garside <geoff@geoffgarside.co.uk>
2009-09-02 04:07:54 +0100

    Improve documentation of Node class

--- lib/demolisher.rb
+++ lib/demolisher.rb
@@ -9,30 +9,47 @@ module Demolisher
     yield node if block_given?
     node
   end
+
+  # Handles all the complexity of accessing the XML contents
   class Node
-    # Creates a new Node object. If the node is not the root node then
-    # the second argument needs to be false.
+    # Creates a new Node object.
+    #
+    # If the node is not the root node then the secondargument needs to be false.
     def initialize(xml, is_root = true)
       @nodes = [xml]
       @nodes.unshift(nil) unless is_root
     end
-    # Access an attribute of the current node
-    # Example:
+
+    # Access an attribute of the current node.
+    #
+    # XML file:
     #   <addressbook>
     #     <person rel="friend">
     #       <firstname>Steve</firstname>
     #     </person>
     #   </addressbook>
-    # 
+    #
+    # Example:
     #   xml.addressbook do
     #     xml.person do
     #       puts "#{xml.firstname} is a #{xml['rel']}"  #=> "Steve is a friend"
     #     end
     #   end
-    # 
+    #
     def [](attr_name)
       @nodes.last.attributes[attr_name]
     end
+
+    # The workhorse, finds the node matching meth.
+    #
+    # Rough flow guide:
+    #   If a block is given then yield to it each for each instance
+    #     of the element found in the current node.
+    #   If no block given then get the first element found
+    #     If the node has only one text element check if the
+    #       method called has a ? suffix then return true if node content
+    #       looks like a boolean. Otherwise return text content
+    #     Otherwise return a new Node instance
     def method_missing(meth, *args, &block) # :nodoc:
       xpath = @nodes.last.find(element_from_symbol(meth))
       return nil if xpath.empty?
@@ -64,13 +81,18 @@ module Demolisher
         end
       end
     end
+
+    # Returns the current nodes contents.
     def to_s # :nodoc:
       @nodes.last.content.strip
     end
+
+    # Transforms a symbol into a XML element path.
     def element_from_symbol(sym) # :nodoc:
       "#{is_root_node? ? '/' : nil}#{sym.to_s.gsub(/[^a-z0-9_]/i, '')}"
     end
-    # Indicates if the current node is the root of the XML document
+
+    # Indicates if the current node is the root of the XML document.
     def is_root_node?
       @nodes.size == 1
     end
